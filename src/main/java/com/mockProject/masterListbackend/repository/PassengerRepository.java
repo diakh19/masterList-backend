cc2c6e3f88c26cb84d1b69bfa4106fc3b83ad44c
diakh19 <diya_khandelwal@optum.com>
2024-06-08 18:34:25 +0530

    handled Duplication

--- src/main/java/com/mockProject/masterListbackend/repository/PassengerRepository.java
+++ src/main/java/com/mockProject/masterListbackend/repository/PassengerRepository.java
@@ -1,7 +1,13 @@
 package com.mockProject.masterListbackend.repository;
 
+import com.mockProject.masterListbackend.Enum.IdType;
 import com.mockProject.masterListbackend.entity.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PassengerRepository extends JpaRepository<Passenger,Long> {
+    Passenger findByIdNumberAndIdType(String idNumber, IdType idType);
 }
--- src/main/java/com/mockProject/masterListbackend/service/impl/PassengerServiceimpl.java
+++ src/main/java/com/mockProject/masterListbackend/service/impl/PassengerServiceimpl.java
@@ -1,7 +1,10 @@
 package com.mockProject.masterListbackend.service.impl;
 
-import com.mockProject.masterListbackend.dto.PassengerDto;
+import com.mockProject.masterListbackend.Enum.IdType;
+import com.mockProject.masterListbackend.dto.RequestDto;
+import com.mockProject.masterListbackend.dto.ResponseDto;
 import com.mockProject.masterListbackend.entity.Passenger;
+import com.mockProject.masterListbackend.exception.DuplicateResourceException;
 import com.mockProject.masterListbackend.exception.ResourceNotFoundException;
 
 import com.mockProject.masterListbackend.repository.PassengerRepository;
@@ -25,47 +28,65 @@ public class PassengerServiceimpl implements PassengerService {
  @Autowired
  private ModelMapper mapper;
     @Override
-    public PassengerDto createPassenger(PassengerDto passengerDto) {
-        Passenger p= this.mapper.map(passengerDto,Passenger.class);
-        Passenger savedPassenger=passengerRepository.save(p);
-        return mapper.map(savedPassenger,PassengerDto.class);
+    public ResponseDto createPassenger(RequestDto requestDto){
+        //checking duplication
+        String idNumber= requestDto.getIdNumber();
+        IdType idType= requestDto.getIdType();
+        Passenger existing=passengerRepository.findByIdNumberAndIdType(idNumber,idType);
+        if(existing!=null){
+            throw new DuplicateResourceException("Duplicate resource");
+        }
+
+        else {
+            Passenger p= mapper.map(requestDto,Passenger.class);
+            Passenger savedPassenger=passengerRepository.save(p);
+            return mapper.map(savedPassenger, ResponseDto.class);
+        }
     }
 
     @Override
-    public PassengerDto getPassengerById(Long passengerId) {
+    public ResponseDto getPassengerById(Long passengerId) {
         Passenger passenger=passengerRepository.findById(passengerId).orElseThrow(()-> new ResourceNotFoundException("Passenger with given id does not exist:"+ passengerId));
-        return mapper.map(passenger,PassengerDto.class);
+        return mapper.map(passenger, ResponseDto.class);
 
     }
 
     @Override
-    public List<PassengerDto> getAllPassengers() {
+    public List<ResponseDto> getAllPassengers() {
         List<Passenger> passengers=passengerRepository.findAll();
-        return passengers.stream().map((passenger)-> mapper.map(passenger,PassengerDto.class)).collect(Collectors.toList());
+        return passengers.stream().map((passenger)-> mapper.map(passenger, ResponseDto.class)).collect(Collectors.toList());
     }
 
     @Override
-    public PassengerDto updatePassenger(Long passengerId, PassengerDto updatedPassenger) {
+    public ResponseDto updatePassenger(Long passengerId, RequestDto updatedPassenger) {
         Passenger passenger=passengerRepository.findById(passengerId).orElseThrow(()-> new ResourceNotFoundException("Passenger with given id does not exist:"+ passengerId));
 
-        passenger.setId(passengerId);
-        passenger.setPassengerType(updatedPassenger.getPassengerType());
-        passenger.setFood(updatedPassenger.getFood());
-        passenger.setDob(updatedPassenger.getDOB());
-        passenger.setBerth(updatedPassenger.getBerth());
-        passenger.setFullName(updatedPassenger.getFullName());
-        passenger.setGender(updatedPassenger.getGender());
-        passenger.setIdType(updatedPassenger.getIdType());
-        passenger.setIdNumber(updatedPassenger.getIdNumber());
-        Passenger updatedPassengerObj=passengerRepository.save(passenger);
-        return mapper.map(updatedPassengerObj,PassengerDto.class);
+        String idNumber=updatedPassenger.getIdNumber();
+        IdType idType=updatedPassenger.getIdType();
+
+        Passenger existing=passengerRepository.findByIdNumberAndIdType(idNumber,idType);
+        if(existing!=passenger && existing!=null){
+            throw new DuplicateResourceException("Update Passenger is not possible.Another passenger with given IdType and IdNumber already exists in List");
+        }
+        else {
+            passenger.setPassengerType(updatedPassenger.getPassengerType());
+            passenger.setFood(updatedPassenger.getFood());
+            passenger.setDob(updatedPassenger.getDOB());
+            passenger.setBerth(updatedPassenger.getBerth());
+            passenger.setFullName(updatedPassenger.getFullName());
+            passenger.setGender(updatedPassenger.getGender());
+            passenger.setIdType(updatedPassenger.getIdType());
+            passenger.setIdNumber(updatedPassenger.getIdNumber());
+            Passenger updatedPassengerObj=passengerRepository.save(passenger);
+            return mapper.map(updatedPassengerObj, ResponseDto.class);
+        }
+
     }
 
     @Override
     public void deletePassenger(Long passengerId) {
         Passenger passenger=passengerRepository.findById(passengerId).orElseThrow(()-> new ResourceNotFoundException("Passenger with given id does not exist:"+ passengerId));
-
         passengerRepository.deleteById(passengerId);
-
     }
+
 }
