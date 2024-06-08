62e7209e782d7c5c18db82fba209f3527dc226ed
diakh19 <diya_khandelwal@optum.com>
2024-06-08 18:25:29 +0530

    modified mapper

--- src/main/java/com/mockProject/masterListbackend/Configuration/MapperConfig.java
+++ src/main/java/com/mockProject/masterListbackend/Configuration/MapperConfig.java
@@ -1,15 +1,18 @@
 package com.mockProject.masterListbackend.Configuration;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfig {
-
     @Bean
-    public ModelMapper mapper()
-    {
-        return new ModelMapper();
-    }
+   public ModelMapper modelMapper() {
+    ModelMapper mapper = new ModelMapper();
+    mapper.getConfiguration()
+            .setMatchingStrategy(MatchingStrategies.STRICT);
+    return mapper;
+   };
+
 }
